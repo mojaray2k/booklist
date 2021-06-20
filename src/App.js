ae94b7209cb0130583c7cf82fd886b94fa4a11f4
Amen Moja Ra <mojaray2k@gmail.com>
2021-06-20 16:12:38 -0400

    storing values as varibles and passing them to html

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 function BookList() {
   return (
-    <section className="booklist">
+    <section className='booklist'>
       <Book />
       <Book />
       <Book />
@@ -13,28 +13,18 @@ function BookList() {
 }
 
 const Book = () => {
+  const title ='Let That Sh*t Go';
+  const author = 'Monica Sweeney'
   return (
-    <article className="book">
-      <Image />
-      <Title />
-      <Author />
+    <article className='book'>
+      <img
+        src='https://images-na.ssl-images-amazon.com/images/I/71hTq4t1PEL._AC_UL200_SR200,200_.jpg'
+        alt=''
+      />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
     </article>
   );
 };
-
-const Image = () => (
-  <img
-    src='https://images-na.ssl-images-amazon.com/images/I/71hTq4t1PEL._AC_UL200_SR200,200_.jpg'
-    alt=''
-  />
-);
-
-const Title = () => (
-  <h1>
-    Let That Sh*t Go:
-  </h1>
-);
-
-const Author = () => <h4 style={{ color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem'}}>Monica Sweeney</h4>
 
 export default BookList;
